540c949aa03b39dae81551459846ebbbabef346c
João Moraski <joaomoraskilunkes@gmail.com>
2024-03-26 15:10:58 -0300

    finalizando todo o crud com js

---
+++ projeto-inicial-js/backend/src/routes.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import UsersController from './controllers/usersController.js'
+
+
+const routes = express.Router();
+
+routes.get('/', (req,res) => {
+    res.send("Home do projeto");
+});
+
+const usersController = new UsersController();
+
+routes.post('/register', async (req,res) => {
+    await usersController.create(req, res)
+    res.send()
+});
+
+routes.get('/users', async (req,res) => {
+    await usersController.getUsers(req, res)
+    res.send()
+});
+
+routes.get('/users/:id', async (req,res) => {
+    const userId = req.params.id; // Pega o id da request
+    await usersController.getUser(userId, req, res)
+    res.send()
+});
+
+routes.delete('/users/:id', async (req, res) => {
+      const userId = req.params.id; // Pega o id da request
+      await usersController.deleteUser(userId, req, res);
+      res.send();
+   
+  });
+  
+  routes.put('/users/:id', async (req, res) => {
+      const userId = req.params.id;
+      const updatedUserData = req.body; // Pega os dados que serao atualizados da request
+      await usersController.updateUser(userId, updatedUserData, req, res);
+      res.send();
+  });
+
+
+export default routes;
--- projeto-inicial-js/backend/src/server.js
+++ projeto-inicial-js/backend/src/server.js
@@ -8,4 +8,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {console.log("lixo de api em node iniciada em 3333")});
+app.listen(3333, () => {console.log("Api iniciada no endereço 3333")});
