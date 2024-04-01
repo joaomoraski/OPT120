7e18a73db371b3282d143cd5b6771bb1a0302032
João Moraski <joaomoraskilunkes@gmail.com>
2024-04-01 19:01:14 -0300

    arrumando umas coisas no front e no back, mas ainda nao funciona o front

--- projeto-inicial-js/frontend/lib/pages/UsuarioPage.dart
+++ projeto-inicial-js/frontend/lib/pages/UsuarioPage.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:frontend/entities/usuario.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(UsuarioApp());
 
 class UsuarioApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: UsuariosScreen(),
     );
@@ -18,38 +18,54 @@ class UsuariosScreen extends StatefulWidget {
   @override
   _UsuariosScreenState createState() => _UsuariosScreenState();
 }
+
 class _UsuariosScreenState extends State<UsuariosScreen> {
-  List<Usuario> _usuarios = [];
+  List<Map<String, dynamic>> _usuarios = [];
+
   @override
   void initState() {
     super.initState();
     _fetchUsuarios();
   }
+
   Future<void> _fetchUsuarios() async {
     final response = await http.get(Uri.http('localhost:3333', '/users'));
     if (response.statusCode == 200) {
       final List<dynamic> json = jsonDecode(response.body);
+      final List<Map<String, dynamic>> users =
+          json.map((user) => user as Map<String, dynamic>).toList();
       setState(() {
-        _usuarios = json.map((item) => Usuario.fromJson(item)).toList();
+        _usuarios = users;
       });
     } else {
-      throw Exception('Failed to load books');
+      throw Exception('Failed to load');
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Usuarios'),
       ),
-      body: ListView.builder(
-        itemCount: _usuarios.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text("${_usuarios[index].id} - ${_usuarios[index].nome}"),
-            subtitle: Text(_usuarios[index].email),
-          );
-        },
+      body: DataTable(
+        headingTextStyle: const TextStyle(
+          fontSize: 16.0,
+          color: Colors.black,
+          fontFamily: 'Inter',
+        ),
+        columns: _usuarios.isNotEmpty
+            ? _usuarios.first.keys
+                .map((key) => DataColumn(label: Text(key.toUpperCase())))
+                .toList()
+            : [],
+        rows: _usuarios
+            .map((item) => DataRow(
+                  cells: item.keys
+                      .map((key) => DataCell(Text(item[key].toString())))
+                      .toList(),
+                ))
+            .toList(),
       ),
     );
   }
